5bfef702d2b71b911a518027c6137fe2a1cc613b
sujoysaha777 <sujoy_saha@tamu.edu>
2016-03-06 01:57:54 +0000

    hw 2 part 2 complete --fixed version

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,10 +13,12 @@ class MoviesController < ApplicationController
   def index
 
      @all_ratings = Movie.all_ratings
-     @set_ratings = params[:ratings]
-     if @set_ratings == nil
+     @checked = params[:ratings]
+     init = Hash.new
+     if @checked == nil
           @set_now = true #condition for first time
-          @set_ratings = Hash.new
+          @all_ratings.each {|x| init[x] = 1}
+          @checked = init
      else 
           @set_now = false  
      end 
@@ -31,6 +33,36 @@ class MoviesController < ApplicationController
         @movies = Movie.all
      end
 
+=begin     
+     @movies = Movie
+     
+     @all_ratings = @movies.all_ratings
+          
+     if session[:checked] == nil    # create new session and set all to true for first time visit
+         session[:checked] = Hash.new
+     	   @all_ratings.each {|x| session[:checked][x] = 1}
+     elsif params[:ratings]  # save current session for user for non-first time visit
+       	 session[:checked] = params[:ratings]     
+     end
+     @checked = session[:checked]
+    
+     if params[:alphabetical_order]
+       session[:alphabetical_order] = params[:alphabetical_order]
+     end
+     
+     @sorted = params[:alphabetical_order]     
+     sort_rate = { 'alphabetical_order': session[:alphabetical_order], 'ratings': session[:checked]}
+     # retrieve only required movies 
+     @movies = Movie.where({rating:  session[:checked].keys })
+     # sort movies
+     if session[:alphabetical_order]
+       @movies = @movies.order(session[:alphabetical_order])
+     end
+     
+     if ( session[:checked] != params[:ratings] || session[:alphabetical_order] != params[:alphabetical_order] )
+       redirect_to(sort_rate)
+     end
+=end
   end
 
   def new
